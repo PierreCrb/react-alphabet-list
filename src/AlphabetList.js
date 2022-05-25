7990bcd912eb7dc1407f5bfe507a3c15030a8c6c
Pierre <pierre.crbs@gmail.com>
2022-05-25 17:57:46 +0200

    fix

--- src/AlphabetList.js
+++ src/AlphabetList.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import AlphabetItem from './AlphabetItem';
+import React, { Component } from "react";
+import AlphabetItem from "./AlphabetItem";
 
 const mapArrToMap = (arr) => {
   const map = new Map();
@@ -7,7 +7,7 @@ const mapArrToMap = (arr) => {
   arr.forEach((item) => {
     let firstChar = item[0];
     if (!Regx.test(firstChar)) {
-      firstChar = '#'
+      firstChar = "#";
     } else {
       firstChar = firstChar.toUpperCase();
     }
@@ -16,12 +16,11 @@ const mapArrToMap = (arr) => {
     } else {
       const arr = map.get(firstChar);
       arr.push(item);
-      map.set(firstChar, arr)
+      map.set(firstChar, arr);
     }
-
-  })
+  });
   return map;
-}
+};
 class AlphabetList extends Component {
   constructor(props) {
     super(props);
@@ -31,84 +30,81 @@ class AlphabetList extends Component {
     if (this.mapPos) {
       this.mapPos.set(id, top);
     }
-  }
+  };
   handleAlphaClick = (char) => {
-    this.scroller.scrollTop = this.mapPos.get(char)
-  }
+    this.scroller.scrollTop = this.mapPos.get(char) - 322;
+  };
   render() {
-    const { generateFn, style, data, className } = this.props;
+    const { generateFn, style, data, className, displayCharIndex } = this.props;
     const map = mapArrToMap(data);
     this.mapPos = new Map();
-    const keyArr = Array.from(map.keys())
+    const keyArr = Array.from(map.keys());
     keyArr.sort();
     return (
       <div
         className={className}
         style={{
-          position: 'absolute',
-          ...(style ? style : {})
+          position: "absolute",
+          ...(style ? style : {}),
         }}
       >
         <div
           style={{
-            width: '100%',
-            height: '100%',
-            overflow: 'auto',
+            width: "100%",
+            height: "100%",
+            overflow: "auto",
             paddingRight: 12,
           }}
-          ref={(ref) => { this.scroller = ref }}
+          ref={(ref) => {
+            this.scroller = ref;
+          }}
         >
-          {
-            keyArr.map((char) => {
-              if (map.get(char) != null) {
-                return (
-                  <AlphabetItem
-                    id={`${char}`}
-                    suffix={` (${map.get(char).length})`}
-                    key={char}
-                    registerPos={this.registerPos}
-                  >
-                    {
-                      map.get(char).map((item, index) => {
-                        return generateFn(item, index);
-                      })
-                    }
-                  </AlphabetItem>
-                )
-              }
-            })
-          }
-
+          {keyArr.map((char) => {
+            if (map.get(char) != null) {
+              return (
+                <AlphabetItem
+                  id={`${char}`}
+                  suffix={` (${map.get(char).length})`}
+                  key={char}
+                  registerPos={this.registerPos}
+                  displayCharIndex={displayCharIndex}
+                >
+                  {map.get(char).map((item, index) => {
+                    return generateFn(item, index);
+                  })}
+                </AlphabetItem>
+              );
+            }
+          })}
         </div>
         <div
           style={{
-            position: 'absolute',
+            position: "absolute",
             top: 12,
             right: 12,
-            color: '#AAA'
+            color: "#AAA",
           }}
         >
-          {
-            keyArr.map(item => {
-              return (
-                <div
-                  key={item}
-                  style={{
-                    fontSize: 9.5,
-                    verticalAlign: 'top',
-                    cursor: 'pointer',
-                    color: '#AAA'
-                  }}
-                  onClick={() => { this.handleAlphaClick(item) }}
-                >
-                  {item}
-                </div>
-              )
-            })
-          }
+          {keyArr.map((item) => {
+            return (
+              <div
+                key={item}
+                style={{
+                  fontSize: 9.5,
+                  verticalAlign: "top",
+                  cursor: "pointer",
+                  color: "#AAA",
+                }}
+                onClick={() => {
+                  this.handleAlphaClick(item);
+                }}
+              >
+                {item}
+              </div>
+            );
+          })}
         </div>
       </div>
-
     );
   }
 }
